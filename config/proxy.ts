343149150d539f6af47df7a16dc7e3308cb07747
陈立恒 <chenliheng@youlai.cn>
2023-03-07 23:33:11 +0800

    feat: 接口代理

--- config/proxy.ts
+++ config/proxy.ts
@@ -1,7 +1,7 @@
 export default {
   dev: {
     '/v1/chat': {
-      target: 'http://43.198.72.177:8100',
+      target: 'http://43.198.72.177:8100/',
       changeOrigin: true,
       pathRewrite: { '^': '' },
       headers: {
